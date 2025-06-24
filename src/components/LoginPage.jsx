b16ce82bada890a1f5bffe88ebd3fcb06336dd88
Niranjani <niranjani123@gmail.com>
2025-06-24 13:01:41 +0530

    Add missing LoginPage component

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,14 @@
+// src/components/LoginPage.jsx
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div className="login-container">
+      <h1>Welcome to Crash IQ</h1>
+      <p>Please log in to continue.</p>
+      {/* Add your login UI here */}
+    </div>
+  );
+};
+
+export default LoginPage;
